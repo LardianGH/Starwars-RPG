56da8a499b169b7d34b7589b7e7f899abf92dfe1
Zion Brown <zabs95@gmail.com>
2019-08-05 20:20:43 -0400

    Dynamically loads in characters using name as id, when you click one it is appended down

---
+++ assets/javascript/logic.js
@@ -0,0 +1,54 @@
+var characters = [
+    {
+        name: "Revan", //name gets picture
+        playerUsing: false, //changes to true if player is chosen
+        hp: 200, // health
+        maxAttack: 50, // random num gen * 
+    },
+    {
+      name: "Bastila", //name gets picture
+      playerUsing: false, //changes to true if player is chosen
+      hp: 200, // health
+      maxAttack: 50, // random num gen * 
+  },
+  {
+    name: "Nihilus", //name gets picture
+    playerUsing: false, //changes to true if player is chosen
+    hp: 200, // health
+    maxAttack: 50, // random num gen * 
+}
+]
+
+//on click to activate
+
+//end on click event
+
+console.log(characters[0].name + "'s attack power is: " + characters[0].maxAttack)
+
+$.each(characters, function(i, value) {
+
+    console.log('value: ', value)
+    var charBox = $('<button>').addClass('char-box');
+    charBox.attr("id", characters[i].name );
+    console.log(characters[i].name)
+    $(charBox).text(characters[i]) //creates the word object over the picture
+
+      $("#char-select").append(charBox);
+      
+      
+
+      $("#" + characters[i].name).on("click", function() {
+console.log(characters[i].name)
+//send all others not clicked down
+
+$("#char-select").append("");
+$("#enemies").append(charBox);
+charBox = $('<div>');
+      })
+    });
+
+
+
+    //  Lord of hunger - Nihilus
+    //  Battle meditation - Bastila
+    //  Orbalisk armor - Bane
